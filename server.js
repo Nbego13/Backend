8e822ce52ccce0929ccc5b6ea63371eb99698bfe
Nbego13 <105458879+Nbego13@users.noreply.github.com>
2025-05-24 12:21:45 +0100

    Update server.js

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ let rooms = [];
 //initializs socket,io with https server and cors options
 const io = require('socket.io')(httpServer, {
     cors: {
-        origin: 'https://chatbox-qc5v.onrender.com',
+        origin: "https://chatbox-qc5v.onrender.com",
         methods: ['GET', 'POST'],
         credentials: true
     },
